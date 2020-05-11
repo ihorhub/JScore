e84b80ccb0046bbcea60397b9ab421ef50d55ef3
ihorhub <ihor_babiy@ukr.net>
2020-05-11 13:21:57 +0300

    add project les6 add new task 7,1

---
+++ lesson7.js
@@ -0,0 +1,11 @@
+
+//- Создать произвольный елемент с id = text.
+// Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id="text".
+
+let hider = document.getElementById('hider');
+let text = document.getElementById('text');
+hider.onclick = hide()
+
+function hide() {
+    text.hidden = true;
+}
