8b7a8d2968e0fce538127941eb273d970db1223a
ihorhub <ihor_babiy@ukr.net>
2020-06-30 23:04:45 +0300

    compex Lesson1

---
+++ video.js
@@ -0,0 +1,5 @@
+// navigator.mediaDevices.getUserMedia({video: true}).then(videoStream => {
+//     let video = document.getElementById('videoblock')
+//     video.srcObject = videoStream
+//     video.play()
+// })
