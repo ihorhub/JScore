6a51b0e47845c8d0a0e063f0a9679831b6fe5786
ihorhub <ihor_babiy@ukr.net>
2020-07-15 18:17:03 +0300

    compex Lesson5

--- lesson5.js
+++ lesson5.js
@@ -30,21 +30,80 @@
 //     {/*...*/},
 // ]
 // }
-// function Tag(titleOfTag, action, attrs) {
-//         this.titleOfTag=titleOfTag ||" no title"
-//         this.action=action|| "no action"
-//         this.attrs=attrs|| []
+
+
+// function Tag(titleOfTag, action, attributes = []) {
+//     this.titleOfTag = titleOfTag
+//     this.action = action
+//     this.attributes = attributes
+//     this.titleAtrib = function(titleName){
+//         this.attributes.push(titleName)
+//     }
+//     this.actionAtrib =  function (actionName){
+//         this.attributes.push(actionName)
+//     }
+//     this.actionAtrib =  function (actionName) {
+//         this.attributes.push(actionName)
+//     }
 //
 // }
-// let title='<a>'
-// let action ='Тег <a> является одним из важных элементов HTML и предназначен для создания ссылок.'
-// let attibytes=[
-//     {titleOfAttr: 'accesskey', actionOfAttr: 'Переход к области с помощью комбинации клавиш'},
-//     {titleOfAttr: 'coords', actionOfAttr: 'Устанавливает координаты активной области'},
-//     {titleOfAttr: 'download', actionOfAttr: 'Предлагает скачать указанный по ссылке файл'},
-// ]
-// let a= new Tag(title,action,attibytes)
+
+//     let a = new Tag('<a>', 'tег <a> является одним из важных элементов HTML и предназначен для создания ссы',)
+// a.titleAtrib('href')
+// a.actionAtrib('\'Задает адрес документа, на который следует перейти.\'')
 // console.log(a)
+
+//
+// function Tag(titleOfTag, action, attributes = []) {
+//     this.titleOfTag = titleOfTag
+//     this.action = action
+//     this.attributes = attributes
+//     this.titleAtrib = function(titleName,actionName){
+//         this.attributes.push(titleName,actionName)
+//     }
+//   }
+//
+// let a = new Tag('<a>', 'tег <a> является одним из важных элементов HTML и предназначен для создания ссы',)
+// a.titleAtrib('href','Задает адрес документа, на который следует перейти')
+// a.titleAtrib('target','Имя окна или фрейма, куда браузер будет загружать документ.')
+// console.log(a)
+//
+// let div = new Tag('<div>', 'Элемент <div> является блочным элементом и предназначен для выделения фрагмента документа с целью изменения вида содержимого',)
+// div.titleAtrib('align',' Задает выравнивание содержимого тега <div>')
+// div.titleAtrib('title',' Добавляет всплывающую подсказку к содержимому.')
+// console.log(div)
+//
+// let h1 = new Tag('<h1>', 'тег <h1> представляет собой наиболее важный заголовок первого уровня',)
+// h1.titleAtrib('align','Определяет выравнивание заголовка.')
+// h1.titleAtrib('dir','адает направление и отображение текста — слева направо или справа налево')
+// console.log(h1)
+//
+// let span = new Tag('<span>', 'Тег <span> предназначен для определения строчных элементов документа',)
+// span.titleAtrib('accesskey','Позволяет получить доступ к элементу с помощью заданного сочетания клавиш')
+// span.titleAtrib('class','Определяет имя класса, которое позволяет связать тег со стилевым оформлением.')
+// console.log(span)
+//
+// let input = new Tag('<input>', 'Главным образом <input> предназначен для создания текстовых полей, различных кнопок, переключателей и флажков')
+// input.titleAtrib('src','Адрес графического файла для поля с изображением')
+// input.titleAtrib('type','Сообщает браузеру, к какому типу относится элемент формы.')
+// console.log(input)
+//
+// let form = new Tag('<form>', 'Тег <form> устанавливает форму на веб-странице.',)
+// form.titleAtrib('accept-charset','Устанавливает кодировку, в которой сервер может принимать и обрабатывать данные.')
+// form.titleAtrib('action','Адрес программы или документа, который обрабатывает данные формы')
+// console.log(form)
+//
+// let option = new Tag('<option>', 'Тег <option> определяет отдельные пункты списка, создаваемого с помощью контейнера <select>.',)
+// option.titleAtrib('disabled','Заблокировать для доступа элемент списка.')
+// option.titleAtrib('label','Указание метки пункта списка.')
+// console.log(option)
+//
+// let select = new Tag('<select>', 'Тег <select> позволяет создать элемент интерфейса в виде раскрывающегося списка, а также список с одним или множественным выбором',)
+// select.titleAtrib('autofocus','Устанавливает, что список получает фокус после загрузки страницы.')
+// select.titleAtrib('multiple','Позволяет одновременно выбирать сразу несколько элементов списка.')
+// console.log(select)
+
+
 
 
 // Створити класс  для об'єкту який описує теги
@@ -82,31 +141,61 @@
 //
 //         ]
 //
-//
+
 //
 //    }
 
-// class Tag{
-//     constructor(titleOfTag, action, attrs) {
-//         this.titleOfTag=titleOfTag ||" no title"
-//         this.action=action|| "no action"
-//         this.attrs=attrs|| []
-//
+// class Tag {
+//     constructor(titleOfTag, action, attributes = []) {
+//         this.titleOfTag = titleOfTag
+//         this.action = action
+//         this.attributes = attributes
+//         this.titleAtrib = function(titleName,actionName){
+//         this.attributes.push(titleName,actionName)
+//     }
 //     }
 // }
 //
 //
-//
-// let title='<a>'
-// let action ='Тег <a> является одним из важных элементов HTML и предназначен для создания ссылок.'
-// let attibytes=[
-//     {titleOfAttr: 'accesskey', actionOfAttr: 'Переход к области с помощью комбинации клавиш'},
-//     {titleOfAttr: 'coords', actionOfAttr: 'Устанавливает координаты активной области'},
-//     {titleOfAttr: 'download', actionOfAttr: 'Предлагает скачать указанный по ссылке файл'},
-// ]
-// let a= new Tag(title,action,attibytes)
+//  let a = new Tag('<a>', 'tег <a> является одним из важных элементов HTML и предназначен для создания ссы',)
+// a.titleAtrib('href','Задает адрес документа, на который следует перейти')
+// a.titleAtrib('target','Имя окна или фрейма, куда браузер будет загружать документ.')
 // console.log(a)
-
+//
+// let div = new Tag('<div>', 'Элемент <div> является блочным элементом и предназначен для выделения фрагмента документа с целью изменения вида содержимого',)
+// div.titleAtrib('align',' Задает выравнивание содержимого тега <div>')
+// div.titleAtrib('title',' Добавляет всплывающую подсказку к содержимому.')
+// console.log(div)
+//
+// let h1 = new Tag('<h1>', 'тег <h1> представляет собой наиболее важный заголовок первого уровня',)
+// h1.titleAtrib('align','Определяет выравнивание заголовка.')
+// h1.titleAtrib('dir','адает направление и отображение текста — слева направо или справа налево')
+// console.log(h1)
+//
+// let span = new Tag('<span>', 'Тег <span> предназначен для определения строчных элементов документа',)
+// span.titleAtrib('accesskey','Позволяет получить доступ к элементу с помощью заданного сочетания клавиш')
+// span.titleAtrib('class','Определяет имя класса, которое позволяет связать тег со стилевым оформлением.')
+// console.log(span)
+//
+// let input = new Tag('<input>', 'Главным образом <input> предназначен для создания текстовых полей, различных кнопок, переключателей и флажков')
+// input.titleAtrib('src','Адрес графического файла для поля с изображением')
+// input.titleAtrib('type','Сообщает браузеру, к какому типу относится элемент формы.')
+// console.log(input)
+//
+// let form = new Tag('<form>', 'Тег <form> устанавливает форму на веб-странице.',)
+// form.titleAtrib('accept-charset','Устанавливает кодировку, в которой сервер может принимать и обрабатывать данные.')
+// form.titleAtrib('action','Адрес программы или документа, который обрабатывает данные формы')
+// console.log(form)
+//
+// let option = new Tag('<option>', 'Тег <option> определяет отдельные пункты списка, создаваемого с помощью контейнера <select>.',)
+// option.titleAtrib('disabled','Заблокировать для доступа элемент списка.')
+// option.titleAtrib('label','Указание метки пункта списка.')
+// console.log(option)
+//
+// let select = new Tag('<select>', 'Тег <select> позволяет создать элемент интерфейса в виде раскрывающегося списка, а также список с одним или множественным выбором',)
+// select.titleAtrib('autofocus','Устанавливает, что список получает фокус после загрузки страницы.')
+// select.titleAtrib('multiple','Позволяет одновременно выбирать сразу несколько элементов списка.')
+// console.log(select)
 
 //- Створити об'єкт car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
 //
@@ -126,11 +215,12 @@
 //     year: '2019',
 //     maxSpeed: 210,
 //     powerKw: 100,
-//     drive: function () {
+//
+//     drive (){
 //         console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`)
 //
 //     },
-//     info: function () {
+//     info () {
 //         if (this.shafer){
 //         console.log(`
 //             model:  ${this.model},
@@ -150,26 +240,26 @@
 //             `)
 //         }
 //     },
-//     increaseMaxSpeed: function (newSpeed) {
+//     increaseMaxSpeed (newSpeed) {
 //         this.maxSpeed += newSpeed
 //
 //     },
-//     changeYear: function (newValue) {
+//     changeYear (newValue) {
 //         this.year = newValue
 //
 //     },
-//     addDriver: function (driver) {
+//       addDriver (driver) {
 //         this.shafer = driver
 //
 //     }
 // }
 // let driver ={name: ' ivan ivanovich', age:45}
-//
+// car.drive()
 // car.increaseMaxSpeed(50)
 // car.changeYear(2020);
 // car.addDriver(driver);
 // car.info();
-// car.drive();
+//
 
 
 // Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
@@ -185,9 +275,10 @@
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і доавляет його в поточний об'єкт car
 
 
+
 // let driver = {name: ' ivan ivanovich', age: 45}
-//
 // function Car(model, manufacturer, year, maxSpeed, powerKw) {
+//
 //     this.model = model ,
 //         this.manufacturer = manufacturer ,
 //         this.year = year ,
@@ -231,9 +322,9 @@
 //         }
 // }
 //
-// let car = new Car('tiguan', 'Volkdwqgen', 2015, 198, 136)
-// car.increaseMaxSpeed(50)
+// let car = new Car('tiguan', 'Volkdwqgen', 2019, 198, 136)
 //
+// car.increaseMaxSpeed(50)
 // car.changeYear(2020);
 // car.addDriver(driver);
 // car.info();
@@ -309,12 +400,11 @@
 //
 // let car = new Car('tiguan', 'Volkdwqgen', 2015, 198, 136)
 // car.increaseMaxSpeed(50)
-//
 // car.changeYear(2020);
 // car.addDriver(driver);
 // car.info();
 // car.drive();
-//
+
 //
 // -створити класс попелюшка з полями ім'я, вік, розмір ноги
 //
@@ -348,20 +438,15 @@
 //         this.shoeSize = shoeSize
 //     }
 //
-//     findCinderell(array) {
-//         let find = null
+//     findCinderell  (array) {
 //         for (const item of array) {
 //             if (item.footSize === this.shoeSize) {
-//                 find = item
-//
+//           console.log( 'My Cinderella name  '+ item.name)
 //             }
 //         }
-//         if (find) {
-//             console.log('My Cinderella name  ' + find.name)
-//         } else {
-//             console.log('i not find my Cinderella')
-//         }
+//
 //     }
+//
 // }
 //
 // let Cinderella1 = new Cinderella('Mila', 16, 35)
@@ -378,7 +463,6 @@
 // let arrayCinderells = [Cinderella1, Cinderella2, Cinderella3, Cinderella4, Cinderella5, Cinderella6, Cinderella7, Cinderella8, Cinderella9, Cinderella10]
 // let prince = new Prince('Ivan', 21, 36)
 // prince.findCinderell(arrayCinderells)
-
 
 //-створити функцію конструктор попелюшка з полями ім'я, вік, розмір ноги
 //
@@ -404,21 +488,15 @@
 //
 //
 //     this.findCinderell = function (array) {
-//         let find = null
 //         for (const item of array) {
 //             if (item.footSize === this.shoeSize) {
-//                 find = item
+//                 console.log('My Cinderella name  ' + item.name)
 //
 //             }
-//         }
-//         if (find) {
-//             console.log('My Cinderella name  ' + find.name)
-//         } else {
-//             console.log('i not find my Cinderella')
+//
 //         }
 //     }
 // }
-//
 // let Cinderella1 = new Cinderella('Mila', 16, 35)
 // let Cinderella2 = new Cinderella('Veronika', 17, 35.5)
 // let Cinderella3 = new Cinderella('Karina', 19, 36.5)
