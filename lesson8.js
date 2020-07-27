d04f8c07712b656ab063ef226debb1bc78a0f087
ihorhub <ihor_babiy@ukr.net>
2020-07-27 17:45:34 +0300

    new 8 lesson

--- lesson8.js
+++ lesson8.js
@@ -102,109 +102,109 @@
 // --Каждому контакту добавить кнопку редактироваиня.
 // При нажати на нее появляется форма, в которой есть все необходимые инпуты для редактирования,
 // которые уже заполнены данными объекта
-let Array_User = 'Array_User'
-let tempUser = {}
-let conent = document.getElementById('content')
-let form1 = document.forms.form1
-form1.submit1.onclick = ev => {
-    //  ev.preventDefault()
-    let person = {...tempUser}
-    tempUser = {}
-    for (let i = 0; i < form1.children.length; i++) {
-        const form1Element = form1.children[i];
-        if (form1Element.name && form1Element.type !== 'submit') {
-            person[form1Element.name] = form1Element.value
-        }
-    }
-    if (!person.id) {
-        person.id = new Date().getTime()
-    }
-    console.log('person')
-    console.log(person)
-    saveUser(person)
-}
-getDataFromLSt()
-
-function saveUser(user) {
-    if (localStorage.hasOwnProperty(Array_User)) {
-        let arrayUsers = JSON.parse(localStorage.getItem(Array_User))
-        let find = arrayUsers.find(value => value.id === user.id)
-        if (find) {
-            let filter = arrayUsers.filter(value => value.id !== user.id)
-            filter.push(user)
-            localStorage.setItem(Array_User, JSON.stringify(filter))
-        } else {
-            arrayUsers.push(user)
-            localStorage.setItem(Array_User, JSON.stringify(arrayUsers))
-        }
-
-    } else {
-        localStorage.setItem(Array_User, JSON.stringify([user]))
-    }
-
-}
-
-function getDataFromLSt() {
-    if (localStorage.hasOwnProperty(Array_User)) {
-        let arrUser = JSON.parse(localStorage.getItem(Array_User))
-        for (const user of arrUser) {
-            conent.appendChild(createDivperson(user))
-        }
-    }
-}
-
-function createDivperson(user) {
-    let main = document.createElement('div')
-    let flag = true
-    for (const key in user) {
-        if (flag) {
-            let h3 = document.createElement('h3')
-            h3.innerText = key + " : " + user[key]
-            main.appendChild(h3)
-            flag = false
-        } else {
-            let p = document.createElement('p')
-            p.innerText = key + " : " + user[key]
-            main.appendChild(p)
-        }
-    }
-    main.style = "width:300px;border:red 1px solid; float:left"
-    let b1 = document.createElement('button')
-    let b2 = document.createElement('button')
-    b1.innerText = 'Edite'
-    b2.innerText = 'Delete'
-    b1.onclick = function () {
-        editUser(user.id)
-    }
-    b2.onclick = function () {
-        deleteUser(user.id)
-    }
-    main.appendChild(b1)
-    main.appendChild(b2)
-    return main
-}
-
-function deleteUser(id) {
-    let parse = JSON.parse(localStorage.getItem(Array_User))
-    let filter = parse.filter(user => user.id !== id)
-    localStorage.setItem(Array_User, JSON.stringify(filter))
-    location.reload()
-}
-
-function editUser(id) {
-    let parse = JSON.parse(localStorage.getItem(Array_User))
-    let user = parse.find(user => user.id === id)
-    for (let i = 0; i < form1.children.length; i++) {
-        const form1Element = form1.children[i];
-        if (form1Element.name && form1Element.type !== 'submit') {
-            for (const key in user) {
-                if (form1Element.name === key) {
-                    form1Element.value = user[key]
-                }
-
-            }
-        }
-
-    }
-    tempUser = user
-}
+// let Array_User = 'Array_User'
+// let tempUser = {}
+// let conent = document.getElementById('content')
+// let form1 = document.forms.form1
+// form1.submit1.onclick = ev => {
+//     //  ev.preventDefault()
+//     let person = {...tempUser}
+//     tempUser = {}
+//     for (let i = 0; i < form1.children.length; i++) {
+//         const form1Element = form1.children[i];
+//         if (form1Element.name && form1Element.type !== 'submit') {
+//             person[form1Element.name] = form1Element.value
+//         }
+//     }
+//     if (!person.id) {
+//         person.id = new Date().getTime()
+//     }
+//     console.log('person')
+//     console.log(person)
+//     saveUser(person)
+// }
+// getDataFromLSt()
+//
+// function saveUser(user) {
+//     if (localStorage.hasOwnProperty(Array_User)) {
+//         let arrayUsers = JSON.parse(localStorage.getItem(Array_User))
+//         let find = arrayUsers.find(value => value.id === user.id)
+//         if (find) {
+//             let filter = arrayUsers.filter(value => value.id !== user.id)
+//             filter.push(user)
+//             localStorage.setItem(Array_User, JSON.stringify(filter))
+//         } else {
+//             arrayUsers.push(user)
+//             localStorage.setItem(Array_User, JSON.stringify(arrayUsers))
+//         }
+//
+//     } else {
+//         localStorage.setItem(Array_User, JSON.stringify([user]))
+//     }
+//
+// }
+//
+// function getDataFromLSt() {
+//     if (localStorage.hasOwnProperty(Array_User)) {
+//         let arrUser = JSON.parse(localStorage.getItem(Array_User))
+//         for (const user of arrUser) {
+//             conent.appendChild(createDivperson(user))
+//         }
+//     }
+// }
+//
+// function createDivperson(user) {
+//     let main = document.createElement('div')
+//     let flag = true
+//     for (const key in user) {
+//         if (flag) {
+//             let h3 = document.createElement('h3')
+//             h3.innerText = key + " : " + user[key]
+//             main.appendChild(h3)
+//             flag = false
+//         } else {
+//             let p = document.createElement('p')
+//             p.innerText = key + " : " + user[key]
+//             main.appendChild(p)
+//         }
+//     }
+//     main.style = "width:300px;border:red 1px solid; float:left"
+//     let b1 = document.createElement('button')
+//     let b2 = document.createElement('button')
+//     b1.innerText = 'Edite'
+//     b2.innerText = 'Delete'
+//     b1.onclick = function () {
+//         editUser(user.id)
+//     }
+//     b2.onclick = function () {
+//         deleteUser(user.id)
+//     }
+//     main.appendChild(b1)
+//     main.appendChild(b2)
+//     return main
+// }
+//
+// function deleteUser(id) {
+//     let parse = JSON.parse(localStorage.getItem(Array_User))
+//     let filter = parse.filter(user => user.id !== id)
+//     localStorage.setItem(Array_User, JSON.stringify(filter))
+//     location.reload()
+// }
+//
+// function editUser(id) {
+//     let parse = JSON.parse(localStorage.getItem(Array_User))
+//     let user = parse.find(user => user.id === id)
+//     for (let i = 0; i < form1.children.length; i++) {
+//         const form1Element = form1.children[i];
+//         if (form1Element.name && form1Element.type !== 'submit') {
+//             for (const key in user) {
+//                 if (form1Element.name === key) {
+//                     form1Element.value = user[key]
+//                 }
+//
+//             }
+//         }
+//
+//     }
+//     tempUser = user
+// }
