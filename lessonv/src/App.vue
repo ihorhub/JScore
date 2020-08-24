055f0d71891fb87253a353e477e41cb804525af9
ihorhub <ihor_babiy@ukr.net>
2020-08-24 14:05:43 +0300

    lesson 111 vue first task

---
+++ lessonv/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+
+    <input type="text" v-model="items">
+    {{items}}
+      <ul v-for="(item,index) in items " :key="index"></ul>
+      <buton @click="additem()"> ADD</buton>
+      <buton v-if="arr.length!==0" @click="removeitem()"> REMOVE</buton>
+
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+    data() {
+        return {
+            arr : [],
+            items: ''
+        }
+      }
+    }
+    methods:{
+      additem(){
+             this.arr.push(this.items)
+        }
+
+    },
+{
+    removeitem()
+    {
+
+        this.arr.slice(this.index)
+    }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
