2accf25394381cc0260255e06d22880b72050f00
ihorhub <ihor_babiy@ukr.net>
2020-09-25 13:00:25 +0300

    home task details

---
+++ Angular/todolist/user-detalies/src/app/components/users/users.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from "../../models/user";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+@Input()
+user:User
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Angular/todolist/user-detalies/src/app/models/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address:object;
+  street:string |number;
+  suite:string|number;
+  city:string
+
+}
