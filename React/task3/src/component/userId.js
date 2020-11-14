004434ce1551e78aeba440b71cfee05c424d230c
ihorhub <ihor_babiy@ukr.net>
2020-11-14 12:59:48 +0200

    new change

--- React/task3/src/component/userId.js
+++ React/task3/src/component/userId.js
@@ -14,23 +14,26 @@ class UserId extends Component {
             <div>
                 <form onSubmit={this.onFormSub}>
                    <input type='number' min={1} max={10} placeholder={'enter id'} onChange={this.searchUserId} />
+                   <button>saerch</button>
                 </form>
                 { user.map(value =>(<UserIdList userId={value} key={value.id}/>))}
             </div>
         );
     }
 
-    // onFormSub=(e)=> {e.preventDefault()
-    //
-    // }
-
-    searchUserId =(ev)=> {
-       this._apiUserId.getUserById(ev.target.value).then(value => {this.setState({user:value})})
- // this.setState({id:ev})
+    onFormSub=(e)=> {e.preventDefault()
 
     }
-    // componentDidMount() {this._apiUserId.getUserById(this.state.id).then(value => {this.setState({user:value})})
-    // }
+
+    searchUserId =(ev)=> {
+       // this._apiUserId.getUserById(ev.target.value).then(value => {this.setState({user:value})})
+ this.setState({id:ev.target.value})
+        console.log(ev.target.value)
+        console.log(this.state.id)
+
+    }
+    componentDidMount() {this._apiUserId.getUserById(this.state.id).then(value => {this.setState({user:value})})
+    }
 }
 
 export default UserId;
