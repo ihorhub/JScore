633d00c0c11708e95ea8c988a4686eb9ca48d5e5
ihorhub <64549617+ihorhub@users.noreply.github.com>
2021-04-14 22:17:38 +0300

    Update joi-user.validator.js

--- Node/lesson9/validators/user/joi-user.validator.js
+++ Node/lesson9/validators/user/joi-user.validator.js
@@ -1,8 +1,14 @@
 const Joi = require('joi');
 const { EMAIL, PASSWORD } = require('../../configs/regexp');
+const girlSubScheme = Joi.array().items(
+  Joi.object({
+    name: Joi.string().alphanum().max(20)
+  })
+);
 
 module.exports = Joi.object({
     name: Joi.string().alphanum().min(2).max(50).required(),
     email: Joi.string().regex(EMAIL).required(),
     password: Joi.string().regex(PASSWORD).required(),
+     girls: girlSubScheme.optional().when('car', { is: true, then: Joi.required() })
 });
